8cf8b42f84912d2277363f01b34abd260a584180
Bartosz Baran <bartosz.baran192@gmail.com>
2022-03-06 12:36:24 +0100

    corses models, admin

--- app/config/settings.py
+++ app/config/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'courses.apps.CoursesConfig',
 ]
 
 MIDDLEWARE = [
